a5571645157bad23cf7b0c5df959232da11c31cb
Hamid Ijaz <chhamidijaz@gmail.com>
2025-03-27 12:49:17 +0500

    fix CORS and added logs

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ const io = require("socket.io")(httpServer, {
 io.use(authSocket);
 io.on("connection", (socket) => socketServer(socket));
 
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -43,7 +50,6 @@ httpServer.listen(process.env.PORT || 4000, () => {
 });
 
 app.use(express.json());
-app.use(cors());
 app.use(
   cors({
     origin: [
@@ -52,8 +58,11 @@ app.use(
       "https://postit-one-swart.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use("/api/posts", posts);
 app.use("/api/users", users);
 app.use("/api/comments", comments);
