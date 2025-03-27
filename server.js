732c2ddea0bf7bddd10972095db72b4088c8200c
Hamid Ijaz <chhamidijaz@gmail.com>
2025-03-27 13:13:51 +0500

    update server.js

--- server.js
+++ server.js
@@ -14,17 +14,38 @@ const Post = require("./models/Post");
 
 dotenv.config();
 
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://postit-quick-tweet.vercel.app",
+    "https://postit-one-swart.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+app.use("/api/posts", posts);
+app.use("/api/users", users);
+app.use("/api/comments", comments);
+app.use("/api/messages", messages);
+
 const httpServer = require("http").createServer(app);
+
 const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://postit-quick-tweet.vercel.app",
-      "https://postit-one-swart.vercel.app",
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+  cors: corsOptions,
 });
 
 io.use(authSocket);
@@ -37,36 +58,9 @@ io.on("connection", (socket) => {
   });
 });
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.error("MongoDB connection error:", error));
-
 httpServer.listen(process.env.PORT || 4000, () => {
   console.log("Listening");
 });
-
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://postit-quick-tweet.vercel.app",
-      "https://postit-one-swart.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use("/api/posts", posts);
-app.use("/api/users", users);
-app.use("/api/comments", comments);
-app.use("/api/messages", messages);
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
